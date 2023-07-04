63aca02617dd1bc7b51b2656dacddf4a1f9eb126
Vladislav <demonhpru@gmail.com>
2023-07-04 11:20:06 +0500

    add auth controller

---
+++ Server/Controllers/authController.js
@@ -0,0 +1,16 @@
+class authController{
+    async signUp(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json({"error": error.message});
+        }
+    }
+    async signIn(req, res){
+        try {
+            
+        } catch (error) {
+            res.status(500).json({"error": error.message});
+        }
+    }
+}
