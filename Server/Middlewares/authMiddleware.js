0af7252930ddcfcfc8072bf23183dcbdd3e2e996
Vladislav <demonhpru@gmail.com>
2023-07-06 22:06:33 +0500

    add authMiddleware

---
+++ Server/Middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+
+export default function(req, res, next){
+    if (req.method === "OPTIONS"){
+        next();
+    }
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token){
+            return res.status(403).json({message: "User not autherizated"})
+        }
+
+        const decodedData = jwt.verify(token, process.env.SECRET_KEY || "SECRET_KEY");
+        req.user = decodedData;
+        next();
+
+    } catch (error) {
+        return res.status(403).json({message: "User not autherizated"})
+    }
+}
